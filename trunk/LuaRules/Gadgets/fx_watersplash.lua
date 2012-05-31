5653ee7a29ae2ae9f50f159875493b8742560bc1
springjools@gmail.com <springjools@gmail.com@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2012-05-31 00:06:30 +0000

    Updated commander selection widget and cleaned away commented Echo commands from the rest.

--- trunk/LuaRules/Gadgets/fx_watersplash.lua
+++ trunk/LuaRules/Gadgets/fx_watersplash.lua
@@ -36,7 +36,6 @@ else
 			Spring.SpawnCEG(splashCEG1, px+random(-aoe/2,aoe/2), py, pz+random(-aoe/2,aoe/2))
 			if aoe > 40 then Spring.SpawnCEG(splashCEG2, px, 0, pz) end
 			Spring.PlaySoundFile(sndWater,15.0,px,0,pz)
-			--Spring.Echo("Explosion!", weaponID, ownerID, Spring.GetGroundInfo(px,pz),aoe)
 			return true
 		else
 			return false
@@ -50,19 +49,10 @@ else
 	
 		for id,Def in pairs(WeaponDefs) do
 			local weaponID
-			-- for i,v in Def:pairs() do
-				-- Spring.Echo(id,i,v)
-			-- end
 			if Def["damageAreaOfEffect"] ~= nil and Def["damageAreaOfEffect"] > 15 and Def["myGravity"] > 0 then
 				weaponID = Def["id"]
 				Script.SetWatchWeapon(weaponID, true)
-				--Spring.Echo(Def["name"] .. " added: AOE = " .. Def["damageAreaOfEffect"] .. " Gravity =" .. Def["myGravity"] )
 			end
 		end
-
-
-		--local bombDefID = WeaponDefNames["TheNameOfMyAwesomeBombWeapon"].id -- change to suit :P
-		--Script.SetWatchWeapon(bombDefID, true)
-		--en
 	end
 end
