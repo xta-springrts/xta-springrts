a80a4c7598b8f56e5d81e193a7924c785415a723
tobiwan.kentobi <tobiwan.kentobi@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2012-07-11 00:39:38 +0000

    reduce redundancy among light definitions

--- LuaRules/Configs/gfx_dynamic_lighting_defs.lua
+++ LuaRules/Configs/gfx_dynamic_lighting_defs.lua
@@ -1,19 +1,50 @@
-local scales = {0.25, 0.25, 0.25} -- specular RGB scales
-local lights = {
+local function CopyTable(srcTbl, dstTbl)
+	assert(dstTbl ~= nil)
+
+	for key, val in pairs(srcTbl) do
+		assert(type(key) ~= type({}))
+
+		if (type(val) == type({})) then
+			dstTbl[key] = {}
+
+			srcSubTbl = val
+			dstSubTbl = dstTbl[key]
+
+			CopyTable(srcSubTbl, dstSubTbl)
+		else
+			dstTbl[key] = val
+		end
+	end
+end
+
+
+local rgbSpecMults = {0.25, 0.25, 0.25} -- specular RGB scales
+local copyLightDefs = {
 	["XTA"] = {
-		WeaponLightDefs = {
+		["core_disintegrator"] = "arm_disintegrator",
+		["core_intimidator"  ] = "arm_berthacannon",
+		["cor_bats"          ] = "arm_bats",
+		["armmine5"          ] = "crawl_blast",
+		["atomic_blastpen"   ] = "atomic_blast",
+		["crblmssl"          ] = "nuclear_missile",
+		["cortron_weapon"    ] = "armemp_weapon",
+	},
+}
+local dynLightDefs = {
+	["XTA"] = {
+		weaponLightDefs = {
 			-- Core Pillager (main barrel) projectiles
 			["core_artillery"] = {
 				projectileLightDef = {
-					diffuseColor    = {4.9,             4.5,             0.2            },
-					specularColor   = {4.9 * scales[1], 4.5 * scales[2], 0.2 * scales[3]},
+					diffuseColor    = {4.9,                   4.5,                   0.2                  },
+					specularColor   = {4.9 * rgbSpecMults[1], 4.5 * rgbSpecMults[2], 0.2 * rgbSpecMults[3]},
 					radius          = 275.0,
 					priority        = 3 * 10,
 					ttl             = 100000,
 				},
 				explosionLightDef = {
-					diffuseColor      = {4.0,             2.5,             0.2            },
-					specularColor     = {4.0 * scales[1], 2.5 * scales[2], 0.2 * scales[3]},
+					diffuseColor      = {4.0,                   2.5,                   0.2                  },
+					specularColor     = {4.0 * rgbSpecMults[1], 2.5 * rgbSpecMults[2], 0.2 * rgbSpecMults[3]},
 					radius            = 350.0,
 					priority          = 3 * 10 + 1,
 					ttl               = 75,
@@ -24,15 +55,15 @@ local lights = {
 			-- Core Goliath (main barrel) projectiles
 			["cor_gol"] = {
 				projectileLightDef = {
-					diffuseColor    = {2.8,             2.8,             0.4            },
-					specularColor   = {0.8 * scales[1], 0.8 * scales[2], 0.1 * scales[3]},
+					diffuseColor    = {2.8,                   2.8,                   0.4                  },
+					specularColor   = {0.8 * rgbSpecMults[1], 0.8 * rgbSpecMults[2], 0.1 * rgbSpecMults[3]},
 					radius          = 250.0,
 					priority        = 3 * 10,
 					ttl             = 100000,
 				},
 				explosionLightDef = {
-					diffuseColor      = {3.0,             2.5,             0.25            },
-					specularColor     = {3.0 * scales[1], 2.5 * scales[2], 0.25 * scales[3]},
+					diffuseColor      = {3.0,                   2.5,                   0.25                  },
+					specularColor     = {3.0 * rgbSpecMults[1], 2.5 * rgbSpecMults[2], 0.25 * rgbSpecMults[3]},
 					radius            = 325.0,
 					priority          = 3 * 10 + 1,
 					ttl               = 2.5 * Game.gameSpeed,
@@ -48,22 +79,10 @@ local lights = {
 			--   projectile triggers a new explosion for
 			--   every frame it is alive (which consumes
 			--   too many light slots)
-			--
-			--   ttl values are arbitrarily large to make
-			--   the lights survive until projectiles die
 			["arm_disintegrator"] = {
 				projectileLightDef = {
-					diffuseColor      = {3.0,             2.0,             0.1            },
-					specularColor     = {3.0 * scales[1], 2.0 * scales[2], 0.1 * scales[3]},
-					radius            = 200.0,
-					priority          = 2 * 10,
-					ttl               = 100000,
-				},
-			},
-			["core_disintegrator"] = {
-				projectileLightDef = {
-					diffuseColor      = {3.0,             2.0,             0.1            },
-					specularColor     = {3.0 * scales[1], 2.0 * scales[2], 0.1 * scales[3]},
+					diffuseColor      = {3.0,                   2.0,                   0.1                  },
+					specularColor     = {3.0 * rgbSpecMults[1], 2.0 * rgbSpecMults[2], 0.1 * rgbSpecMults[3]},
 					radius            = 200.0,
 					priority          = 2 * 10,
 					ttl               = 100000,
@@ -72,22 +91,11 @@ local lights = {
 
 
 			-- explodeas/selfdestructas lights for various large units
-			["armmine5"] = {
-				explosionLightDef = {
-					diffuseColor      = { 6.5,              5.5,              3.25        },
-					specularColor     = { 6.5 * scales[1],  5.5 * scales[2],  3.25 * scales[3]},
-					priority          = 12 * 10,
-					radius            = 725.0,
-					ttl               = 2.5 * Game.gameSpeed,
-					decayFunctionType = {0.0, 0.0, 0.0},
-					altitudeOffset    = 45.0,
-				},
-			},
 			["commander_blast"] = {
 				explosionLightDef = {
-					diffuseColor      = { 9.0,              8.0,              3.5            },
-					specularColor     = { 9.0 * scales[1],  8.0 * scales[2],  3.5 * scales[3]},
-					priority          = 14 * 10,
+					diffuseColor      = { 9.0,                    8.0,                    3.5                  },
+					specularColor     = { 9.0 * rgbSpecMults[1],  8.0 * rgbSpecMults[2],  3.5 * rgbSpecMults[3]},
+					priority          = 15 * 10,
 					radius            = 1050.0,
 					ttl               = 3 * Game.gameSpeed,
 					decayFunctionType = {0.0, 0.0, 0.0},
@@ -96,41 +104,43 @@ local lights = {
 			},
 			["giant_blast"] = {
 				explosionLightDef = {
-					diffuseColor      = { 7.0,              6.0,              3.5        },
-					specularColor     = { 7.0 * scales[1],  6.0 * scales[2],  3.5 * scales[3]},
-					priority          = 13 * 10,
+					diffuseColor      = { 7.0,                    6.0,                    3.5                  },
+					specularColor     = { 7.0 * rgbSpecMults[1],  6.0 * rgbSpecMults[2],  3.5 * rgbSpecMults[3]},
+					priority          = 14 * 10,
 					radius            = 775.0,
 					ttl               = 2.5 * Game.gameSpeed,
 					decayFunctionType = {0.0, 0.0, 0.0},
 					altitudeOffset    = 55.0,
 				},
 			},
+
 			["crawl_blast"] = {
 				explosionLightDef = {
-					diffuseColor      = { 6.5,              5.5,              3.25            },
-					specularColor     = { 6.5 * scales[1],  5.5 * scales[2],  3.25 * scales[3]},
-					priority          = 12 * 10,
+					diffuseColor      = { 6.5,                    5.5,                    3.25                  },
+					specularColor     = { 6.5 * rgbSpecMults[1],  5.5 * rgbSpecMults[2],  3.25 * rgbSpecMults[3]},
+					priority          = 13 * 10,
 					radius            = 725.0,
+					ttl               = 2.5 * Game.gameSpeed,
+					decayFunctionType = {0.0, 0.0, 0.0},
+					altitudeOffset    = 45.0,
+				},
+			},
+			["crawl_blastsml"] = {
+				explosionLightDef = {
+					diffuseColor      = { 5.5,                    4.5,                    2.25                  },
+					specularColor     = { 5.5 * rgbSpecMults[1],  4.5 * rgbSpecMults[2],  2.25 * rgbSpecMults[3]},
+					priority          = 12 * 10,
+					radius            = 675.0,
 					ttl               = 2.5 * Game.gameSpeed,
 					decayFunctionType = {0.0, 0.0, 0.0},
 					altitudeOffset    = 35.0,
 				},
 			},
+
 			["atomic_blast"] = {
 				explosionLightDef = {
-					diffuseColor      = { 6.0,              5.0,              4.5            },
-					specularColor     = { 6.0 * scales[1],  5.0 * scales[2],  4.5 * scales[3]},
-					priority          = 11 * 10,
-					radius            = 575.0,
-					ttl               = 2 * Game.gameSpeed,
-					decayFunctionType = {0.0, 0.0, 0.0},
-					altitudeOffset    = 25.0,
-				},
-			},
-			["atomic_blastpen"] = {
-				explosionLightDef = {
-					diffuseColor      = { 6.0,              5.0,              4.5            },
-					specularColor     = { 6.0 * scales[1],  5.0 * scales[2],  4.5 * scales[3]},
+					diffuseColor      = { 6.0,                    5.0,                    4.5                  },
+					specularColor     = { 6.0 * rgbSpecMults[1],  5.0 * rgbSpecMults[2],  4.5 * rgbSpecMults[3]},
 					priority          = 11 * 10,
 					radius            = 575.0,
 					ttl               = 2 * Game.gameSpeed,
@@ -140,8 +150,8 @@ local lights = {
 			},
 			["atomic_blastsml"] = {
 				explosionLightDef = {
-					diffuseColor      = { 5.0,              4.0,              3.5            },
-					specularColor     = { 5.0 * scales[1],  4.0 * scales[2],  3.5 * scales[3]},
+					diffuseColor      = { 5.0,                    4.0,                    3.5                  },
+					specularColor     = { 5.0 * rgbSpecMults[1],  4.0 * rgbSpecMults[2],  3.5 * rgbSpecMults[3]},
 					priority          = 11 * 10,
 					radius            = 575.0,
 					ttl               = 2 * Game.gameSpeed,
@@ -159,33 +169,15 @@ local lights = {
 			--   ttl value roughly matches CEG duration
 			["nuclear_missile"] = {
 				projectileLightDef = {
-					diffuseColor    = {5.0,             5.0,             0.0            },
-					specularColor   = {5.0 * scales[1], 5.0 * scales[2], 0.0 * scales[3]},
+					diffuseColor    = {5.0,                   5.0,                   0.0                  },
+					specularColor   = {5.0 * rgbSpecMults[1], 5.0 * rgbSpecMults[2], 0.0 * rgbSpecMults[3]},
 					priority        = 20 * 10,
 					radius          = 1000.0,
 					ttl             = 100000,
 				},
 				explosionLightDef = {
-					diffuseColor      = {45.0,             45.0,             30.0            },
-					specularColor     = {45.0 * scales[1], 45.0 * scales[2], 30.0 * scales[3]},
-					priority          = 20 * 10 + 1,
-					radius            = 2500.0,
-					ttl               = 8.5 * Game.gameSpeed,
-					decayFunctionType = {0.0, 0.0, 0.0},
-					altitudeOffset    = 250.0,
-				},
-			},
-			["crblmssl"] = {
-				projectileLightDef = {
-					diffuseColor    = {5.0,             5.0,             0.0            },
-					specularColor   = {5.0 * scales[1], 5.0 * scales[2], 0.0 * scales[3]},
-					priority        = 20 * 10,
-					radius          = 1000.0,
-					ttl             = 100000,
-				},
-				explosionLightDef = {
-					diffuseColor      = {45.0,             45.0,             30.0            },
-					specularColor     = {45.0 * scales[1], 45.0 * scales[2], 30.0 * scales[3]},
+					diffuseColor      = {45.0,                   45.0,                   30.0                  },
+					specularColor     = {45.0 * rgbSpecMults[1], 45.0 * rgbSpecMults[2], 30.0 * rgbSpecMults[3]},
 					priority          = 20 * 10 + 1,
 					radius            = 2500.0,
 					ttl               = 8.5 * Game.gameSpeed,
@@ -197,33 +189,15 @@ local lights = {
 			-- Arm Stunner / Core Neutron (small nuke) projectiles
 			["armemp_weapon"] = {
 				projectileLightDef = {
-					diffuseColor    = {3.0,             3.0,             0.0            },
-					specularColor   = {3.0 * scales[1], 3.0 * scales[2], 0.0 * scales[3]},
+					diffuseColor    = {3.0,                   3.0,                   0.0                  },
+					specularColor   = {3.0 * rgbSpecMults[1], 3.0 * rgbSpecMults[2], 0.0 * rgbSpecMults[3]},
 					priority        = 8 * 10,
 					radius          = 400.0,
 					ttl             = 100000,
 				},
 				explosionLightDef = {
-					diffuseColor      = {12.0,             12.0,             8.0            },
-					specularColor     = {12.0 * scales[1], 12.0 * scales[2], 8.0 * scales[3]},
-					priority          = 8 * 10 + 1,
-					radius            = 575.0,
-					ttl               = 2 * Game.gameSpeed,
-					decayFunctionType = {0.0, 0.0, 0.0},
-					altitudeOffset    = 125.0,
-				},
-			},
-			["cortron_weapon"] = {
-				projectileLightDef = {
-					diffuseColor    = {3.0,             3.0,             0.0            },
-					specularColor   = {3.0 * scales[1], 3.0 * scales[2], 0.0 * scales[3]},
-					priority        = 8 * 10,
-					radius          = 400.0,
-					ttl             = 100000,
-				},
-				explosionLightDef = {
-					diffuseColor      = {12.0,             12.0,             8.0            },
-					specularColor     = {12.0 * scales[1], 12.0 * scales[2], 8.0 * scales[3]},
+					diffuseColor      = {12.0,                   12.0,                   8.0                  },
+					specularColor     = {12.0 * rgbSpecMults[1], 12.0 * rgbSpecMults[2], 8.0 * rgbSpecMults[3]},
 					priority          = 8 * 10 + 1,
 					radius            = 575.0,
 					ttl               = 2 * Game.gameSpeed,
@@ -240,33 +214,15 @@ local lights = {
 			--   higher priority
 			["arm_berthacannon"] = {
 				projectileLightDef = {
-					diffuseColor    = {3.9,             3.9,             0.2            },
-					specularColor   = {3.9 * scales[1], 3.9 * scales[2], 0.2 * scales[3]},
+					diffuseColor    = {3.9,                   3.9,                   0.2                  },
+					specularColor   = {3.9 * rgbSpecMults[1], 3.9 * rgbSpecMults[2], 0.2 * rgbSpecMults[3]},
 					priority        = 5 * 10,
 					radius          = 475.0,
 					ttl             = 100000,
 				},
 				explosionLightDef = {
-					diffuseColor      = {4.0,             3.0,             1.2            },
-					specularColor     = {4.0 * scales[1], 3.0 * scales[2], 1.2 * scales[3]},
-					priority          = 5 * 10 + 1,
-					radius            = 500.0,
-					ttl               = 2 * Game.gameSpeed,
-					decayFunctionType = {0.0, 0.0, 0.0},
-					altitudeOffset    = 50.0,
-				},
-			},
-			["core_intimidator"] = {
-				projectileLightDef = {
-					diffuseColor    = {3.9,             3.9,             0.2            },
-					specularColor   = {3.9 * scales[1], 3.9 * scales[2], 0.2 * scales[3]},
-					priority        = 5 * 10,
-					radius          = 475.0,
-					ttl             = 100000,
-				},
-				explosionLightDef = {
-					diffuseColor      = {4.0,             3.0,             1.2            },
-					specularColor     = {4.0 * scales[1], 3.0 * scales[2], 1.2 * scales[3]},
+					diffuseColor      = {4.0,                   3.0,                   1.2                  },
+					specularColor     = {4.0 * rgbSpecMults[1], 3.0 * rgbSpecMults[2], 1.2 * rgbSpecMults[3]},
 					priority          = 5 * 10 + 1,
 					radius            = 500.0,
 					ttl               = 2 * Game.gameSpeed,
@@ -278,33 +234,15 @@ local lights = {
 			-- Arm Millennium / Core Warlord (Bertha) projectiles
 			["arm_bats"] = {
 				projectileLightDef = {
-					diffuseColor    = {3.9,             3.9,             0.2            },
-					specularColor   = {3.9 * scales[1], 3.9 * scales[2], 0.2 * scales[3]},
+					diffuseColor    = {3.9,                   3.9,                   0.2                  },
+					specularColor   = {3.9 * rgbSpecMults[1], 3.9 * rgbSpecMults[2], 0.2 * rgbSpecMults[3]},
 					priority        = 5 * 10,
 					radius          = 475.0,
 					ttl             = 100000,
 				},
 				explosionLightDef = {
-					diffuseColor      = {4.0,             3.0,             1.2            },
-					specularColor     = {4.0 * scales[1], 3.0 * scales[2], 1.2 * scales[3]},
-					priority          = 5 * 10 + 1,
-					radius            = 500.0,
-					ttl               = 2 * Game.gameSpeed,
-					decayFunctionType = {0.0, 0.0, 0.0},
-					altitudeOffset    = 50.0,
-				},
-			},
-			["cor_bats"] = {
-				projectileLightDef = {
-					diffuseColor    = {3.9,             3.9,             0.2        },
-					specularColor   = {3.9 * scales[1], 3.9 * scales[2], 0.2 * scales[3]},
-					priority        = 5 * 10,
-					radius          = 475.0,
-					ttl             = 100000,
-				},
-				explosionLightDef = {
-					diffuseColor      = {4.0,             3.0,             1.2            },
-					specularColor     = {4.0 * scales[1], 3.0 * scales[2], 1.2 * scales[3]},
+					diffuseColor      = {4.0,                   3.0,                   1.2                  },
+					specularColor     = {4.0 * rgbSpecMults[1], 3.0 * rgbSpecMults[2], 1.2 * rgbSpecMults[3]},
 					priority          = 5 * 10 + 1,
 					radius            = 500.0,
 					ttl               = 2 * Game.gameSpeed,
@@ -316,5 +254,20 @@ local lights = {
 	},
 }
 
-return lights
+local modLightDefs = dynLightDefs[Game.modShortName]
+local modCopyDefs = copyLightDefs[Game.modShortName]
+
+-- insert copy-definitions for each light that has one
+if (modLightDefs ~= nil and modCopyDefs ~= nil) then
+	for dstWeaponDef, srcWeaponDef in pairs(modCopyDefs) do
+		modLightDefs.weaponLightDefs[dstWeaponDef] = {}
+
+		srcLightDefTbl = modLightDefs.weaponLightDefs[srcWeaponDef]
+		dstLightDefTbl = modLightDefs.weaponLightDefs[dstWeaponDef]
+
+		CopyTable(srcLightDefTbl, dstLightDefTbl)
+	end
+end
+
+return dynLightDefs
 
