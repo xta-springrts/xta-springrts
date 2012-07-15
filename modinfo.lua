64e2f346196d58dcfebfe432a75e7f2edde15063
tobiwan.kentobi <tobiwan.kentobi@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2012-07-15 19:43:23 +0000

    update modinfo URL

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,12 @@
 return {
   name='XTA',
   description='XTA Version 9.0 series',
-  URL='http://taspring.clan-sy.com/',
+  URL='http://code.google.com/p/xta-springrts/',
   shortname='XTA',
   version='9 SVN',
   mutator='',
   game='Total Annihilation',
   shortGame='TA',
   modtype=1,
-
 }
+
