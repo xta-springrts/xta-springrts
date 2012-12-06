2854c6c7f13303f6f0ed91191805ef5507e25b73
webmaster <webmaster@hktresnjevka.hr>
2012-12-06 22:55:57 +0000

    Updated Projectile lights widget and added a blacklist for weapons that use dynamic lights so no double lights

---
+++ LuaUI/Configs/gfx_projectile_lights_defs.lua
@@ -0,0 +1,32 @@
+local BlackList = {
+	["cor_gol"             ] = true,
+	["corplas_weapon"      ] = true,
+	["corkrog_fire"        ] = true, -- FIXME? should be krogoth_artillery but corkrog_fire has plasma shell CEGs?
+--	["krogoth_artillery"   ] = true,
+	["commander_self_d"    ] = true,
+	["arm_udisintegrator"  ] = true,
+	["csarm_disintegrator" ] = true,
+	["core_disintegrator"  ] = true,
+	["core_udisintegrator" ] = true,
+	["cscore_disintegrator"] = true,
+	["core_intimidator"    ] = true,
+	["cor_bats"            ] = true,
+	["armmine5"            ] = true,
+	["atomic_blastpen"     ] = true,
+	["crblmssl"            ] = true,
+	["cortron_weapon"      ] = true,
+	["corvipe_laser"       ] = true,
+	["corkrog_head"        ] = true,
+	["core_artillery"      ] = true,
+	["commander_blast"     ] = true,
+	["arm_disintegrator"   ] = true,
+	["arm_berthacannon"    ] = true,
+	["arm_bats"            ] = true,
+	["armsnipe_weapon"     ] = true,
+	["crawl_blast"         ] = true,
+	["atomic_blast"        ] = true,
+	["nuclear_missile"     ] = true,
+	["armemp_weapon"       ] = true,
+}
+
+return BlackList
