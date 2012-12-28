f1dcf850352e3a23e7cf77f870c0a672e31fad03
springjools <springjools@gmail.com>
2012-12-28 17:34:06 +0000

    Change to not show nametags on decoys for allies.

--- LuaUI/Widgets/unit_comm_nametags.lua
+++ LuaUI/Widgets/unit_comm_nametags.lua
@@ -84,7 +84,7 @@ function widget:DrawWorld()
 	local cp 		= unitDef.customParams or nil
     local height    = unitDef.height+heightOffset
 	 
-	if unitDef and cp and cp.iscommander then
+	if unitDef and cp and cp.iscommander and not cp.isdecoycommander then
       DrawUnitPlayerName(unitID, height)
     end
   end
