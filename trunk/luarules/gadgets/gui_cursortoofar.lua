b60f967dc051c7015dba6d1b2bafd8c2668f7d36
webmaster@hktresnjevka.hr <webmaster@hktresnjevka.hr@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2013-03-04 23:38:33 +0000

    Seems one has to replace the mouse cursor EVERY video frame. This fixes the problem for 91.0, for 92.0 I'll wait for engine side fix http://springrts.com/mantis/view.php?id=3520

--- trunk/luarules/gadgets/gui_cursortoofar.lua
+++ trunk/luarules/gadgets/gui_cursortoofar.lua
@@ -49,22 +49,18 @@ local spAssignMouseCursor 	= Spring.AssignMouseCursor
 
 else	--	UNSYNCED
 
-if Game.version > "91.0" then return end	-- not needed in Spring > 91.0
+--if Game.version > "91.0" then return end	-- not needed in Spring > 91.0
 
-local spGetGameFrame 		= Spring.GetGameFrame
 local spGetUnitDefID 		= Spring.GetUnitDefID
 local spSetMouseCursor 		= Spring.SetMouseCursor
 local spGetActiveCommand 	= Spring.GetActiveCommand
 local spGetSelectedUnits 	= Spring.GetSelectedUnits
 local spTraceScreenRay 		= Spring.TraceScreenRay
 local spGetMouseState 		= Spring.GetMouseState
-local frame = 0
 
 	function gadget:Update()
 		local _, activeCmdID = spGetActiveCommand()
-		local gf = spGetGameFrame()
-		if (activeCmdID == CMD_ATTACK) and (frame < gf) then
-			frame = gf
+		if activeCmdID == CMD_ATTACK then
 			local mx, my = spGetMouseState()
 			local sU = spGetSelectedUnits()
 			local s, t = spTraceScreenRay(mx,my)
