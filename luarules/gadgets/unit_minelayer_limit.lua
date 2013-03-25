fad78bc51996e8038b0ef432c5112216ec22667c
springjools <springjools@gmail.com>
2013-03-25 00:36:43 +0000

    Update to minelayer gadget. Now counts finished mines and lets you cancel orders.

--- luarules/gadgets/unit_minelayer_limit.lua
+++ luarules/gadgets/unit_minelayer_limit.lua
@@ -21,18 +21,25 @@ if (gadgetHandler:IsSyncedCode()) then
 ----------------------
 -- SYNCED CODE 		--
 ----------------------
-	local minelayerDefTable = {}
+
+	local InsertUnitCmdDesc 				= Spring.InsertUnitCmdDesc
+	local EditUnitCmdDesc					= Spring.EditUnitCmdDesc
+	local GetUnitCommands					= Spring.GetUnitCommands
+	local minelayerDefTable 				= {} -- table with minelayer unitdefID:s
+	local mineDefTable						= {}
 	
-	local minelayerUnits = {
-        arm_podger = true,
-        core_spoiler = true,
+	local minelayerStock					= {} -- table with the stock of mines by minelayer unitid 
+	local pendingMines						= {} -- table by builderID of pending mines
+	
+	local mineDescID 						= 3 -- just a random command desc ID that works. Programmer is too stoopid/lazy to find out the right way to do it.
+	
+	local minelayerNames = {				-- table of minelayer unit names
+		arm_podger = true,
+		core_spoiler = true,
 		arm_valiant = true,
 		core_limiter = true,
-        }
-
-	local mineTable = {}
-
-	local mineNames = {
+		}	
+	local mineNames = {						-- table of mine unit names
 		-- arm_drip = true, exclude minesweepers
 		-- arm_fart_mine = true, exclude minesweepers
 		-- arm_fart_spidey = true, exclude sweepers
@@ -59,47 +66,49 @@ if (gadgetHandler:IsSyncedCode()) then
 		-- core_wave = true, exclude minesweepers
 	}	
 	
-	local minedescarm= {
-	name 			= MINEAMOUNT_ARM .. " mines left",
-	action			= "",
-	disabled  		= true,
-	id				= CMD_NIL,
-	type			= CMDTYPE.ICON_AREA,
-	tooltip			= "Stock of mines: " .. MINEAMOUNT_ARM,
-	cursor			= "cursorattack",
-	texture     	= 'unitpics/arm_genmine.png'
-	}
-	
-	local minedesccore= {
-	name 			= MINEAMOUNT_CORE .. " mines left",
-	action			= "",
-	disabled  		= true,
-	id				= CMD_NIL,
-	type			= CMDTYPE.ICON_AREA,
-	tooltip			= "Stock of mines: " .. MINEAMOUNT_CORE,
-	cursor			= "cursorattack",
-	texture     	= 'unitpics/core_genmine.png'
-	}
-	
-	local minedescwater= {
-	name 			= MINEAMOUNT_WATER .. " mines left",
-	action			= "",
-	disabled  		= true,
-	id				= CMD_NIL,
-	type			= CMDTYPE.ICON_AREA,
-	tooltip			= "Stock of mines: " .. MINEAMOUNT_WATER,
-	cursor			= "cursorattack",
-	texture     	= 'unitpics/water_genmine.png'
-	}
-	
-	local mineDescID = 3 -- just a random command desc ID that works. Programmer is too stoopid/lazy to find out the right way to do it.
+	local MD_arm	= 	{
+		name 			= MINEAMOUNT_ARM .. " mines left",
+		action			= "",
+		disabled  		= true,
+		id				= CMD_NIL,
+		type			= CMDTYPE.ICON_AREA,
+		tooltip			= "Stock of mines: " .. MINEAMOUNT_ARM,
+		cursor			= "cursorattack",
+		texture     	= 'unitpics/arm_genmine.png'
+		}	
+	local MD_core		= {
+		name 			= MINEAMOUNT_CORE .. " mines left",
+		action			= "",
+		disabled  		= true,
+		id				= CMD_NIL,
+		type			= CMDTYPE.ICON_AREA,
+		tooltip			= "Stock of mines: " .. MINEAMOUNT_CORE,
+		cursor			= "cursorattack",
+		texture     	= 'unitpics/core_genmine.png'
+		}
+	local MD_water		= {
+		name 			= MINEAMOUNT_WATER .. " mines left",
+		action			= "",
+		disabled  		= true,
+		id				= CMD_NIL,
+		type			= CMDTYPE.ICON_AREA,
+		tooltip			= "Stock of mines: " .. MINEAMOUNT_WATER,
+		cursor			= "cursorattack",
+		texture     	= 'unitpics/water_genmine.png'
+		}
+		
 	
 	function gadget:Initialize()
 		for id,unitDef in pairs(UnitDefs) do
 			local uName = unitDef.name
-			if minelayerUnits[uName] then
+			if minelayerNames[uName] then
 				minelayerDefTable[unitDef.id] = true
 			end
+			
+			if mineNames[uName] then
+				mineDefTable[unitDef.id] = true
+			end
+			
         end
 	end
 
@@ -113,78 +122,130 @@ if (gadgetHandler:IsSyncedCode()) then
 		end
 	end
 	
-	function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)	
-		if cmdID < 0 and UnitDefs[-cmdID] then
+	local function countMineQueue(unitID, limit)
+		local queue = GetUnitCommands(unitID)
+		local mines = 0
+		for _, cmd in pairs(queue) do
+			if mineDefTable[-cmd["id"]] then 
+				mines = mines + 1
+				if mines > limit then break end
+			end
+		end
+		--Echo("Mines pending: ",mines,"/",limit)
+		return mines
+	end
+	
+	local function isOrderCancellation(unitID, cmdID, cmdParams)
+		local queue = GetUnitCommands(unitID)
+		local x = cmdParams[1]
+		local y = cmdParams[2]
+		local z = cmdParams[3]
+		
+		for i, cmd in pairs(queue) do
+			local params = cmd["params"]
+			if cmd["id"] == cmdID and params[1] == x and params[2] == y and params[3] == z then
+				return true
+			end
+		end
+		return false
+	end
+
+	function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
+		
+		if cmdID < 0 and mineDefTable[-cmdID] then
 			if mineNames[UnitDefs[-cmdID].name] then
-				if mineTable[unitID] > 0 then
-					mineTable[unitID] = mineTable[unitID] - 1
-					local tex = ""
-					if getSide(UnitDefs[unitDefID].name) == "arm" then
-						tex = 'unitpics/arm_genmine.png'
-					elseif getSide(UnitDefs[unitDefID].name) == "core" then
-						tex = 'unitpics/core_genmine.png'
-					else
-						tex = 'unitpics/water_genmine.png'
-					end
-					
-					if mineTable[unitID] == 1 then
-						Spring.EditUnitCmdDesc(unitID, mineDescID, {
-								name 			= "1 mine left",
-								action			= "",
-								disabled  		= true,
-								id				= CMD_NIL,
-								type			= CMDTYPE.ICON_AREA,
-								tooltip			= "Stock of mines: " .. mineTable[unitID],
-								cursor			= "cursorattack",
-								texture     	= tex
-						})
-					else
-						Spring.EditUnitCmdDesc(unitID, mineDescID, {
-								name 			= mineTable[unitID] .. " mines left",
-								action			= "",
-								disabled  		= true,
-								id				= CMD_NIL,
-								type			= CMDTYPE.ICON_AREA,
-								tooltip			= "Stock of mines:" .. mineTable[unitID],
-								cursor			= "cursorattack",
-								texture     	= tex
-						})
-					end
-					--Echo("Mine built!", UnitDefs[-cmdID].name)
-					return true
-				else
-					Echo(UnitDefs[unitDefID].humanName..": Mine storage empty.")
+				local stock = minelayerStock[unitID]
+				if stock == 0 then
 					return false
+				else
+					local pendingMines = countMineQueue(unitID, stock)					
+					if  stock > pendingMines then
+						return true
+					elseif stock == pendingMines then
+						if isOrderCancellation(unitID, cmdID, cmdParams) then
+							return true
+						else
+							return false
+						end
+					else
+						return false
+					end
 				end
 			end
 		end
 		return true
 	end
 
-
 	function gadget:UnitCreated(unitID, unitDefID, teamID, builderID) 
 		if minelayerDefTable[unitDefID] then
 			if getSide(UnitDefs[unitDefID].name) == "arm" then
-				mineTable[unitID] = MINEAMOUNT_ARM
-				Spring.InsertUnitCmdDesc(unitID, mineDescID, minedescarm)
+				minelayerStock[unitID] = MINEAMOUNT_ARM
+				InsertUnitCmdDesc(unitID, mineDescID, MD_arm)
 			elseif getSide(UnitDefs[unitDefID].name) == "core" then
-				mineTable[unitID] = MINEAMOUNT_CORE
-				Spring.InsertUnitCmdDesc(unitID, mineDescID, minedesccore)
+				minelayerStock[unitID] = MINEAMOUNT_CORE
+				InsertUnitCmdDesc(unitID, mineDescID, MD_core)
 			else
-				mineTable[unitID] = MINEAMOUNT_WATER
-				Spring.InsertUnitCmdDesc(unitID, mineDescID, minedescwater)
+				minelayerStock[unitID] = MINEAMOUNT_WATER
+				InsertUnitCmdDesc(unitID, mineDescID, MD_water)
 			end
-			
+		end
+		
+		if mineDefTable[unitDefID] then
+			pendingMines[unitID] = builderID
 		end
 	end
 
 	function gadget:UnitFinished(unitID, unitDefID, teamID)
+		local builderID = pendingMines[unitID] or nil 
 		
+		if builderID then
+			if minelayerStock[builderID] > 0 then
+				minelayerStock[builderID] = minelayerStock[builderID] -1
+				pendingMines[unitID] = nil
+				
+				local builderDefID = Spring.GetUnitDefID(builderID)
+				local tex = ""
+				local caption
+				
+				-- get current stock for button caption
+				if minelayerStock[builderID] == 0 then
+					caption = "No mines"
+				elseif minelayerStock[builderID] == 1 then
+					caption = "1 mine left"
+				else
+					caption = minelayerStock[builderID].. " mines left"
+				end
+					
+				-- get minelayer side for caption
+				if getSide(UnitDefs[builderDefID].name) == "arm" then
+					tex = 'unitpics/arm_genmine.png'
+				elseif getSide(UnitDefs[builderDefID].name) == "core" then
+					tex = 'unitpics/core_genmine.png'
+				else
+					tex = 'unitpics/water_genmine.png'
+				end
+				
+				-- change button caption
+				EditUnitCmdDesc(builderID, mineDescID, {
+					name 			= caption,
+					action			= "",
+					disabled  		= true,
+					id				= CMD_NIL,
+					type			= CMDTYPE.ICON_AREA,
+					tooltip			= "Stock of mines:" .. minelayerStock[builderID],
+					cursor			= "cursorattack",
+					texture     	= tex
+					}
+				)
+									
+				--Echo("Mine built!", UnitDefs[unitDefID].humanName)
+			end
+		end
 	end
 
 	function gadget:UnitDestroyed(unitID, unitDefID, teamID, attackerID, attackerDefID, attackerTeamID)
 		if minelayerDefTable[unitDefID] then
-			mineTable[unitID] = nil
+			minelayerStock[unitID] = nil
 		end
 	end
 	
