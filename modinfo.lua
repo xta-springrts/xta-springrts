cd6820a6870f27871a515e239d7e939bf475a00c
springjools <springjools@gmail.com>
2013-04-05 11:15:18 +0000

    Change version tag from '9 SVN' to '$VERSION'. For being compatible with rapid.

--- modinfo.lua
+++ modinfo.lua
@@ -3,7 +3,7 @@ return {
   description='XTA Version 9.0 series',
   URL='http://code.google.com/p/xta-springrts/',
   shortname='XTA',
-  version='9 SVN',
+  version='$VERSION',
   mutator='',
   game='Total Annihilation',
   shortGame='TA',
