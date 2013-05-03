85b4bd32c9966923d38d1cc2f3062a00b560157d
springjools@gmail.com <springjools@gmail.com@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2013-05-03 23:00:35 +0000

    Set commander as member of group number 1 on gamestart and unload widget.

---
+++ trunk/luaui/widgets/unit_commander_autogroup.lua
@@ -0,0 +1,43 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+--  file:    	snd_volume_osd.lua
+--  brief:   	volume control OSD
+-- version: 	1.2
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function widget:GetInfo()
+  return {
+    name      = "Commander autogroup",
+    desc      = "Sets Commander as member of group 1 at game start",
+    author    = "Jools",
+    date      = "Apr, 2013",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = false  --  loaded by default?
+  }
+end
+
+local GetLocalTeamID 		= 	Spring.GetLocalTeamID
+local GetTeamUnits 			= 	Spring.GetTeamUnits
+local GetUnitDefID 			= 	Spring.GetUnitDefID
+local SetUnitGroup 			= 	Spring.SetUnitGroup
+
+
+function widget:GameStart()
+	local teamID = GetLocalTeamID()
+	local units = GetTeamUnits(teamID)
+	local commanderID = units[1] or nil
+	
+	if commanderID then
+		local cdID = GetUnitDefID(commanderID)
+		if cdID and UnitDefs[cdID] then	
+			local cp = UnitDefs[cdID].customParams
+			if cp and cp.iscommander then
+				SetUnitGroup(commanderID, 1)
+			end
+		end
+	end
+	widgetHandler.RemoveWidget()
+end
