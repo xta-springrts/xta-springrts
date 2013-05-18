c94a69048317fe1e52b76c7efc3428797b4a6035
anmeldekram71 <anmeldekram71@web.de>
2013-05-18 14:00:49 +0000

    added animals that wander around the map. (and a new icontype "blank" used by these critters)
    -critters are invisible on minimap+radar and have a blank icon, as not to be confused with player units
    -enjoy the amazing duck and friends!
    -i tried to not make them interfere with gameplay as much as possible:
    --for now, mostly only on ffa maps where it often has less serious games anyway
    --attacking them is not possible, to avoid units accidently following them around the map
    --critters are placed in ways where they will not get in the way
    --they can be reclaimed instantly (should they block construction)
    --they are killed easily (near explosions etc)
    --but still they CAN decloak cloaked units..not sure how to fix that and how much it matters. if in doubt, reclaim them.
    -they spawn on the maps defined in gaia_critters_config.lua
    
    modoptions "critters" = false to turn them off.
    
    someone please TEST, hope i did not forget any files.
    
    critters appear on:
    Tabula-v2
    Sparewood-v01
    IslandParadiseV2
    Ternion
    TheColdPlace, The Cold Place Remake, The Cold Place Remake V3c
    Frozen_Gauntlet_TNM03-V2
    Epic-EE-CrossingGlade-v04

---
+++ luarules/gadgets/gaia_critters.lua
@@ -0,0 +1,99 @@
+function gadget:GetInfo()
+  return {
+    name      = "gaia critter units",
+    desc      = "units spawn and wander around the map",
+    author    = "knorke",
+    date      = "2013",
+    license   = "horse",
+    layer     = -10,
+    enabled   = true,
+	}
+end
+
+local mo = Spring.GetModOptions()
+--Spring.Echo ("mo.critters:", mo.critters)
+if mo and tonumber (mo.critters)==0 then
+	Spring.Echo ("gaia_critters.lua: turned off via modoptions")
+	gadgetHandler:RemoveGadget(self)
+end
+
+local GaiaTeamID  = Spring.GetGaiaTeamID ()
+
+-- synced only
+if (not gadgetHandler:IsSyncedCode()) then
+	return false
+end
+
+local critterConfig = include("LuaRules/Configs/gaia_critters_config.lua")
+
+local critterUnits = {}	--critter units that are currently alive
+
+function gadget:Initialize()
+	Spring.Echo ("gaia_critters.lua: gadget:Initialize() Game.mapName=" .. Game.mapName)
+	if not critterConfig[Game.mapName] then Spring.Echo ("no critter config for this map") return end	
+	for key, cC in pairs (critterConfig[Game.mapName]) do
+	
+		for unitName, unitAmount in pairs (cC.unitNames) do
+			--Spring.Echo ("***"..unitName .. " " .. unitAmount)			
+			for i=1, unitAmount do
+				local unitID = nil
+				if cC.spawnBox then
+					local x = math.random (cC.spawnBox.x1, cC.spawnBox.x2)
+					local z = math.random (cC.spawnBox.z1, cC.spawnBox.z2)
+					unitID = Spring.CreateUnit (unitName,x,100,z, 0, GaiaTeamID)
+					randomPatrolInBox (unitID, cC.spawnBox)
+				end
+				if cC.spawnCircle then
+					local a = math.rad (math.random (0,360))
+					local r = math.random (0, cC.spawnCircle.r)
+					local x = cC.spawnCircle.x + (math.sin (a)*r)
+					local z = cC.spawnCircle.z + (math.cos (a)*r)
+					unitID = Spring.CreateUnit (unitName,x,100,z, 0, GaiaTeamID)
+					randomPatrolInCircle (unitID, cC.spawnCircle)
+				end
+				--Spring.Echo ("i:"..i)				
+				Spring.SetUnitNeutral (unitID, true)
+				Spring.SetUnitNoSelect (unitID, true)
+				critterUnits[unitID] = true				
+			end			
+		end		
+	end
+end
+
+
+function randomPatrolInBox (unitID, box)
+	for i=1,5 do
+		local x = math.random (box.x1, box.x2)
+		local z = math.random (box.z1, box.z2)
+		Spring.GiveOrderToUnit(unitID, CMD.PATROL , {x, 100, z}, {"shift"})
+	end
+end
+
+function randomPatrolInCircle (unitID, circle)
+	for i=1,5 do
+		local a = math.rad (math.random (0,360))
+		local r = math.random (0, circle.r)
+		local x = circle.x + (math.sin (a)*r)
+		local z = circle.z + (math.cos (a)*r)	
+		Spring.GiveOrderToUnit(unitID, CMD.PATROL , {x, 100, z}, {"shift"})
+	end
+end
+
+function gadget:UnitDestroyed(unitID, unitDefID, teamID, attackerID, attackerDefID, attackerTeamID)
+	if critterUnits[unitID] then critterUnits[unitID] = nil end
+end
+
+--http://springrts.com/phpbb/viewtopic.php?f=23&t=30109
+function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions, cmdTag, synced)	
+	--Spring.Echo (CMD[cmdID] or "nil")
+	if cmdID and cmdID == CMD.ATTACK then 		
+		if cmdParams and #cmdParams == 1 then			
+			--Spring.Echo ("target is unit" .. cmdParams[1] .. " #cmdParams=" .. #cmdParams)
+			if critterUnits[cmdParams[1]] then 
+			--	Spring.Echo ("target is a critter and ignored!") 
+				return false 
+			end
+		end
+	end		
+return true
+end
