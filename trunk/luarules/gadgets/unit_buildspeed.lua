60e6806e509645ee3281630d1d60b4457440cb08
springjools@gmail.com <springjools@gmail.com@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2013-09-10 15:55:32 +0000

    Make gadget unload on error.

--- trunk/luarules/gadgets/unit_buildspeed.lua
+++ trunk/luarules/gadgets/unit_buildspeed.lua
@@ -181,7 +181,8 @@ end
 
 function gadget:AllowUnitBuildStep(builderID, builderTeamID, uID, uDefID, step)
 	if not buildspeedlist[builderID] then
-		--Spring.Echo("Error in unit buildspeed gadget. Variables: ", builderID, buildspeedlist[builderID])
+		Spring.Echo("Error in unit buildspeed gadget. Variables: ", builderID, buildspeedlist[builderID])
+		gadgetHandler:RemoveGadget(self)
 		return nil
 	end
     return (step <= 0) or not (buildspeedlist[builderID].mode==0 and ((teamMetalStalling[builderTeamID] and requiresMetal[uDefID]) or (teamEnergyStalling[builderTeamID] and requiresEnergy[uDefID])))
