d301ec64e74b8ecdffdb5fef5df297955fc5ffd7
springjools <springjools@gmail.com>
2013-09-30 13:48:41 +0000

    Include a gadget for messages when team dies.

---
+++ luarules/configs/gui_teamdiedmessages_defs.lua
@@ -0,0 +1,34 @@
+-- Format: <tn> is replaced with actual team name
+
+
+local messages = {
+	'<tn> has been obliterated.',
+	'<tn> has been liquidated.',
+	'<tn> has been exterminated.',
+	'<tn> has been eradicated.',
+	'<tn> has been annihilated.',
+	'<tn> has been dishabilitated.',
+	'<tn> has been devastated.',
+	'<tn> has been deactivated.',
+	'<tn> has been dominated.',
+	'<tn> has gone to a better place.',
+	'<tn> played Russian Roulette with a semi-automatic rifle.',
+	'<tn> has been deported to Siberia.',
+	'<tn> has mislaid his towel.',
+	'<tn> was (mostly) harmless.',
+	'<tn>: So long, and thanks for all the fish!',
+	'<tn> nipples exploded with delight.',
+	'<tn> is pushing up the daisies.',
+	'<tn> will not buy this record, it is scratched.',
+	'<tn> fought like a dairy farmer.',
+	'<tn> could really use a breath mint.',
+}
+
+local defaultmessages = {
+	'<side> forces have gone to a better place.',
+	'<side> vermin have been exterminated.',
+	'<side> forces have been obliterated.',
+}
+
+return messages, defaultmessages
+
---
+++ luarules/gadgets/gui_teamdiedmessages.lua
@@ -0,0 +1,62 @@
+function gadget:GetInfo()
+  return {
+    name      = "TeamDiedMessages",
+    desc      = "Prints a message when a Team died",
+    author    = "abma",
+    date      = "Sep, 2013",
+    license   = "GNU GPL, v2 or later",
+    layer     = 5,
+    enabled   = true  --  loaded by default?
+  }
+end
+
+if gadgetHandler:IsSyncedCode() then
+	
+	-------------------
+	-- SYNCED PART --
+	-------------------
+	
+	local Echo 							= Spring.Echo
+	local PlaySoundFile					= Spring.PlaySoundFile
+	local GetTeamRulesParam				= Spring.GetTeamRulesParam
+	local messages, defaultmessages 	= include("LuaRules/Configs/gui_teamdiedmessages_defs.lua")
+	
+	function gadget:Initialize()
+	end
+	
+	local function getMsg (attribute)
+		local msg
+		if attribute == 'Arm' or attribute == 'Core' then
+			msg = defaultmessages[math.random(#defaultmessages)]
+			return string.gsub(msg,'<side>',attribute)
+		elseif attribute and #attribute > 0 then
+			msg = messages[math.random(#messages)]
+			return string.gsub(msg,'<tn>',attribute)
+		else
+			return "The enemy forces have been destroyed."
+		end
+	end
+
+	function gadget:TeamDied(TeamID)
+		local teamID, playerid,_,isAi = Spring.GetTeamInfo(TeamID)
+		local msg
+		
+		if playerid and (not isAi) then
+			local name = Spring.GetPlayerInfo(playerid)
+			msg = getMsg(name)
+		else
+			local startUnit = GetTeamRulesParam(TeamID, 'startUnit')
+			local suName = UnitDefs[startUnit].name
+			
+			if string.sub(suName,1,1) == 'a' then 
+				msg = getMsg('Arm')
+			elseif string.sub(suName,1,1) == 'c' then
+				msg = getMsg('Core')
+			else
+				msg = getMsg('Enemy')
+			end
+		end
+		PlaySoundFile("sounds/beep1.wav",3.0,0,0,0,0,0,0,'userinterface')
+		Echo(msg)
+	end	
+end
