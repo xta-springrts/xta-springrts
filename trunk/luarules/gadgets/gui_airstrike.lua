9644f6e7333a19d84b25b1d507571a765b58d0a7
springjools@gmail.com <springjools@gmail.com@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2013-10-01 22:05:25 +0000

    New cursor for airstrikes, fix cursortoofar to work with spring 95.0

---
+++ trunk/luarules/gadgets/gui_airstrike.lua
@@ -0,0 +1,64 @@
+local versionNumber = "1.0"
+
+function gadget:GetInfo()
+	return {
+	name = "Airstrikes",
+	desc = "Add separate icon for air strikes",
+	author = "Jools",
+	date = "Oct, 2013",
+	license = "tango",
+	layer = 2,
+	enabled = true,
+	}
+end
+
+-- Shared SYNCED/UNSYNCED
+
+local CMD_ATTACK 							= CMD.ATTACK
+local Echo 									= Spring.Echo
+	
+if (gadgetHandler:IsSyncedCode()) then
+
+	--SYNCED
+	local EditUnitCmdDesc						= Spring.EditUnitCmdDesc
+	local FindUnitCmdDesc						= Spring.FindUnitCmdDesc
+	local InsertUnitCmdDesc						= Spring.InsertUnitCmdDesc
+
+	function gadget:Initialize()
+		Spring.AssignMouseCursor("Airstrike", "cursorairstrike", true, false)
+	end
+
+	
+else
+
+--UNSYNCED
+	
+	local GetUnitDefID							= Spring.GetUnitDefID
+	local SetMouseCursor						= Spring.SetMouseCursor
+	local GetActiveCommand						= Spring.GetActiveCommand
+	local GetSelectedUnits						= Spring.GetSelectedUnits
+	
+	
+	function gadget:Update()
+		local _, activeCmdID = GetActiveCommand()
+		
+		if activeCmdID == CMD_ATTACK then
+			local sU = GetSelectedUnits()
+			local onlyBombers = true 
+			
+			for i=1, #sU do
+				local unitID = sU[i]
+				local unitDef = UnitDefs[GetUnitDefID(unitID)]
+				local canFly =  unitDef.canFly
+				local canBomb = unitDef.isBomberAirUnit
+				if not canBomb then 
+					onlyBombers = false
+					break
+				end
+			end
+			if onlyBombers then
+				SetMouseCursor("Airstrike")	
+			end
+		end
+	end	
+end
