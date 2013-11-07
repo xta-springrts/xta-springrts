b7defa1b41b961642ab7cd26b7f65b0e3a1bad2b
springjools@gmail.com <springjools@gmail.com@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2013-11-07 20:36:37 +0000

    Some new cursors.

--- trunk/luaintro/Addons/music.lua
+++ trunk/luaintro/Addons/music.lua
@@ -17,7 +17,7 @@ end
 Spring.SetSoundStreamVolume(0)
 local musicfiles = VFS.DirList(LUA_DIRNAME .. "music", "*.ogg")
 local musicSetting = Spring.GetConfigInt('snd_intromusic') or 0
-if musicSetting ~= 1 then musicSetting == 0 end -- prevent users from inputtting weird data
+if musicSetting ~= 1 then musicSetting = 0 end -- prevent users from inputtting weird data
 Spring.SetConfigInt('snd_intromusic', musicSetting)
 
 if (#musicfiles > 0) and musicSetting == 1 then
