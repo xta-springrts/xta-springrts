3099f23757bca0b7c1a165473570b322547521a5
springjools@gmail.com <springjools@gmail.com@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2014-01-29 02:17:07 +0000

    dbg_featurespawner.lua

---
+++ trunk/luarules/gadgets/dbg_featurespawner.lua
@@ -0,0 +1,92 @@
+function gadget:GetInfo()
+  return {
+	name      = "Featurespawner",
+	desc      = "Places all wrecks and heaps on map",
+	author    = "Jools",
+	date      = "Oct 2013",
+	license   = "Shark",
+	layer     = 0,
+	enabled   = false,
+  }
+end
+
+-- designed for maps like coldplace and bigger. If you use on too small map, spring may crash. Modify "step" variable if needed
+
+local modOptions = Spring.GetModOptions()
+
+if modOptions and modOptions.debugmode then
+	if modOptions.debugmode == '1' then 
+		enable = true 
+	end
+	else
+	if Game.modVersion =='$VERSION' then 
+		enable = true 
+	end
+end
+
+if not enable then 	-- allow spawning info only for SVN version
+	return false
+end
+	 
+if (gadgetHandler:IsSyncedCode()) then
+	-----------------
+	-- SYNCED PART --
+	-----------------
+	
+	local spawnCommand = "spawnfeatures"
+	local Echo = Spring.Echo
+
+	function gadget:Initialize()
+  		gadgetHandler:AddChatAction(spawnCommand, SpawnAllFeatures, "Spawn all features on map")
+	end
+
+	function gadget:Shutdown()
+		gadgetHandler:RemoveChatAction(spawnCommand)
+	end
+	
+	function gadget:GameFrame(f)
+		if f==150 then
+			SpawnAllFeatures()
+		end
+	end	
+	
+	function SpawnAllFeatures()
+		Echo("Spawning all features...")
+		local row, col = 0, 0
+		local x0,z0 = 100,100
+		local mapx,mapz = Game.mapSizeX, Game.mapSizeZ
+		local step = 250 -- modify if needed on smaller map
+		for i, fdef in pairs (FeatureDefs) do
+			local x = x0 + col * step
+			local z 
+			if x > mapx then
+				col = 0
+				row = row + 1 
+			end
+			
+			x = x0 + col * step
+			z = z0 + row * step
+			y = Spring.GetGroundHeight(x,z)
+			
+			if x and y and z then
+				Spring.CreateFeature(fdef.id,x,y,z)
+				--Echo("Feature:",i,x,z,fdef.name)
+				
+				-- gl.PushMatrix()			
+				-- gl.Translate(x,y,z)
+				-- gl.Billboard()			
+				-- gl.Text(fdef.name,0,0,10)
+				-- gl.PopMatrix()
+				
+				if i%2 == 0 then z = z - 40 end
+				
+				Spring.MarkerAddPoint(x,y,z,fdef.name)
+			end
+			col = col + 1
+			if z > mapz then z = 0 end
+		end
+	end
+	
+else
+
+end
