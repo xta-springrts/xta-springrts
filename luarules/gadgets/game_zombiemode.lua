bacf1cb6af7922ba28a9fcc20257fbfe7b4ad762
tobiwan.kentobi <tobiwan.kentobi@gmail.com>
2014-06-18 20:26:20 +0000

    zombies patrol

--- luarules/gadgets/game_zombiemode.lua
+++ luarules/gadgets/game_zombiemode.lua
@@ -21,41 +21,44 @@ local spGetUnitNeutral     = Spring.GetUnitNeutral
 local spSetUnitNeutral     = Spring.SetUnitNeutral
 local spGetGameFrame       = Spring.GetGameFrame
 local spGetUnitPosition    = Spring.GetUnitPosition
+local spGetUnitDirection   = Spring.GetUnitDirection
 local spGetUnitBuildFacing = Spring.GetUnitBuildFacing
 local spCreateUnit         = Spring.CreateUnit
 local spGiveOrderToUnit    = Spring.GiveOrderToUnit
-local Echo              	= Spring.Echo
-local gaiaTeamID			= Spring.GetGaiaTeamID()
 
 local modOptions    = Spring.GetModOptions()
 local modOptionDefs = VFS.Include("modoptions.lua")
+local modRules      = VFS.Include("gamedata/modrules.lua")
+
+local losResMult  = 1.0
+local losMipLevel = 1.0
+local gaiaTeamID  = Spring.GetGaiaTeamID()
 
 -- "modOptions" is a <string, string> map (values are not numbers!)
-local haveZombies = (((modOptions["zombies"] or "0") + 0) ~= 0) or false
+local haveZombies = (((modOptions["zombies"] or "0") + 0) ~= 0)
 local zombieConf  = "LuaRules/Configs/game_zombiemode_defs.lua"
 local zombieDefs  = VFS.FileExists(zombieConf) and include(zombieConf) or {}
 local zombieQueue = {}
 local zombieCount = 0
 
-local dgunWeapons = {		-- better to hardcode these, as many weapons are listed as dgun, for example bogus dgun
-	arm_disintegrator = true,
-	core_disintegrator = true,
-	core_udisintegrator = true,
-	uber_disintegrator = true,
+local dgunTable = { -- better to hardcode these, as many weapons are listed as dgun, for example bogus dgun
+	[WeaponDefNames[  "arm_disintegrator"].id] = true,
+	[WeaponDefNames[ "core_disintegrator"].id] = true,
+	[WeaponDefNames["core_udisintegrator"].id] = true,
+	[WeaponDefNames[ "uber_disintegrator"].id] = true,
 }
-local dgunTable = {} -- Populated from dgunWeapons; a little better to store weapons in table by id instead of name
 
 function gadget:Initialize()
 	assert(modOptionDefs ~= nil)
 
 	if (modOptions == nil) then
-		Echo("[game_zombiemode] no mod-options")
+		Spring.Echo("[game_zombiemode] no mod-options")
 		gadgetHandler:RemoveGadget(self)
 		return
 	end
 
 	if (gaiaTeamID == 0) then
-		Echo("[game_zombiemode] no Gaia-team")
+		Spring.Echo("[game_zombiemode] no Gaia-team")
 		gadgetHandler:RemoveGadget(self)
 		return
 	end
@@ -74,13 +77,11 @@ function gadget:Initialize()
 		gadgetHandler:RemoveGadget(self)
 		return
 	end
-	
-	for id,weaponDef in pairs(WeaponDefs) do
-		local wName = weaponDef.name
-		if dgunWeapons[wName] then
-			dgunTable[weaponDef.id] = true
-		end
-	end	
+
+	if (modRules ~= nil and modRules["sensors"] ~= nil and modRules["sensors"]["los"] ~= nil) then
+		losResMult  = modRules["sensors"]["los"]["losMul"     ]
+		losMipLevel = modRules["sensors"]["los"]["losMipLevel"]
+	end
 end
 
 -- TODO:
@@ -90,7 +91,8 @@ end
 --	 morphing should not trigger respawn *FIXED*
 function gadget:UnitDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
 	local health,_,_,_,buildProgress = Spring.GetUnitHealth(unitID)
-	if health < 0 and buildProgress >= 1 and not dgunTable[weaponDefID] then
+
+	if (health < 0.0 and buildProgress >= 1.0 and not dgunTable[weaponDefID]) then
 		local isZombie = unitTeam == gaiaTeamID
 		local zombieDef = zombieDefs[unitDefID] or {}
 		local canRespawn = zombieDef.canRespawn
@@ -108,6 +110,7 @@ function gadget:UnitDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weap
 			id     = unitID,
 			defID  = unitDefID,
 			pos    = {spGetUnitPosition(unitID)},
+			dir    = {spGetUnitDirection(unitID)},
 			facing = spGetUnitBuildFacing(unitID),
 			frame  = spGetGameFrame() + respawnTime
 		}
@@ -123,6 +126,16 @@ function gadget:GameFrame(n)
 				spGiveOrderToUnit(unitID, CMD.FIRE_STATE, {[1] = CMD_FIRESTATE_FATW}, {})
 				spGiveOrderToUnit(unitID, CMD.MOVE_STATE, {[1] = CMD_MOVESTATE_ROAM}, {})
 				--spSetUnitNeutral(unitID, true)
+
+				-- losRadius = sightDist * losResMult / (SQUARE_SIZE * (1 << losMipLevel))
+				-- sightDist = losRadius * SQUARE_SIZE * math.pow(2, losMipLevel) / losResMult
+				local losRadius = UnitDefs[spawn.defID].losRadius
+				local sightDist = losRadius * 8 * math.pow(2, losMipLevel) / losResMult
+				local patrolVec = {spawn.dir[1] * sightDist * 0.75, 0.0, spawn.dir[3] * sightDist * 0.75}
+
+				if (not UnitDefs[spawn.defID].isImmobile) then
+					spGiveOrderToUnit(unitID, CMD.PATROL, {spawn.pos[1] + patrolVec[1], spawn.pos[2], spawn.pos[3] + patrolVec[3]}, {})
+				end
 			end
 
 			zombieQueue[index] = nil
