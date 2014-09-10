14489ffa4b7cbf01212c889a4683f97038c4ddee
springjools <springjools@gmail.com>
2014-09-10 19:59:48 +0000

    Some fixes to zombiemode, set buildings as neutral etc

--- luarules/configs/game_zombiemode_defs.lua
+++ luarules/configs/game_zombiemode_defs.lua
@@ -13,5 +13,48 @@ for unitDefName, unitDef in pairs(UnitDefs) do
 	}
 end
 
-return zombieDefs
+local captureUnitDefs = {
+	[ UnitDefNames["arm_cappy"].id ] 						= UnitDefNames["arm_cappy"   ],
+	[ UnitDefNames["arm_commander"].id ] 					= UnitDefNames["arm_commander"   ],
+	[ UnitDefNames["arm_decoy_commander"].id ] 				= UnitDefNames["arm_decoy_commander"   ],
+	[ UnitDefNames["arm_decoy_ucommander"].id ] 			= UnitDefNames["arm_decoy_ucommander"   ],
+	[ UnitDefNames["arm_decoy_ucommander_core"].id ] 		= UnitDefNames["arm_decoy_ucommander_core"   ],
+	[ UnitDefNames["arm_nincommander"].id ] 				= UnitDefNames["arm_nincommander"   ],
+	[ UnitDefNames["arm_noruas"].id ] 						= UnitDefNames["arm_noruas"   ],
+	[ UnitDefNames["arm_scommander"].id ] 					= UnitDefNames["arm_scommander"   ],
+	[ UnitDefNames["arm_u0commander"].id ] 					= UnitDefNames["arm_u0commander"   ],
+	[ UnitDefNames["arm_u2commander"].id ] 					= UnitDefNames["arm_u2commander"   ],
+	[ UnitDefNames["arm_u3commander"].id ] 					= UnitDefNames["arm_u3commander"   ],
+	[ UnitDefNames["arm_u4commander"].id ] 					= UnitDefNames["arm_u4commander"   ],
+	[ UnitDefNames["arm_ucommander"].id ] 					= UnitDefNames["arm_ucommander"   ],
+	[ UnitDefNames["armcom"].id ] 							= UnitDefNames["armcom"   ],
+	[ UnitDefNames["commando"].id ] 						= UnitDefNames["commando"   ],
+	[ UnitDefNames["corcom"].id ] 							= UnitDefNames["corcom"   ],
+	[ UnitDefNames["core_cappy"].id ] 						= UnitDefNames["core_cappy"   ],
+	[ UnitDefNames["core_commander"].id ]		 			= UnitDefNames["core_commander"   ],
+	[ UnitDefNames["core_decoy_commander"].id ] 			= UnitDefNames["core_decoy_commander"   ],
+	[ UnitDefNames["core_decoy_ucommander"].id ] 			= UnitDefNames["core_decoy_ucommander"   ],
+	[ UnitDefNames["core_decoy_ucommander_arm"].id ] 		= UnitDefNames["core_decoy_ucommander_arm"   ],
+	[ UnitDefNames["core_nincommander"].id ] 				= UnitDefNames["core_nincommander"   ],
+	[ UnitDefNames["core_scommander"].id ] 					= UnitDefNames["core_scommander"   ],
+	[ UnitDefNames["core_u0commander"].id ] 				= UnitDefNames["core_u0commander"   ],
+	[ UnitDefNames["core_u2commander"].id ] 				= UnitDefNames["core_u2commander"   ],
+	[ UnitDefNames["core_u3commander"].id ] 				= UnitDefNames["core_u3commander"   ],
+	[ UnitDefNames["core_u4commander"].id ] 				= UnitDefNames["core_u4commander"   ],
+	[ UnitDefNames["core_ucommander"].id ] 					= UnitDefNames["core_ucommander"   ],
+	[ UnitDefNames["noruas"].id ] 							= UnitDefNames["noruas"],
+	}
+
+local napUnitDefs = {
+	[ UnitDefNames["arm_atlas"].id ] 						= UnitDefNames["arm_atlas"],
+	[ UnitDefNames["arm_bear"].id ] 						= UnitDefNames["arm_bear"],
+	[ UnitDefNames["arm_hulk"].id ] 						= UnitDefNames["arm_hulk"],
+	[ UnitDefNames["arm_ornith"].id ] 						= UnitDefNames["arm_ornith"],
+	[ UnitDefNames["core_envoy"].id ] 						= UnitDefNames["core_envoy"],
+	[ UnitDefNames["core_turtle"].id ] 						= UnitDefNames["core_turtle"],
+	[ UnitDefNames["core_valkyrie"].id ] 					= UnitDefNames["core_valkyrie"],
+	[ UnitDefNames["core_zeppelin"].id ] 					= UnitDefNames["core_zeppelin"],
+	}
+
+return zombieDefs,captureUnitDefs,napUnitDefs
 
