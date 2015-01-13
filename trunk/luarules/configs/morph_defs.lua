2d7c37aa8869eb95f0ca59a432bd0dc065681c74
johnbaraniecki@hotmail.com <johnbaraniecki@hotmail.com@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2015-01-13 22:18:42 +0000

    Implemented a few units. Added Guardians of Kadesh Race, of the excess version. Teaser version. Not ready for full play but tech 1 is ready.

--- trunk/luarules/configs/morph_defs.lua
+++ trunk/luarules/configs/morph_defs.lua
@@ -99,6 +99,31 @@ local morphDefs = {
 		--tech = 2
 	},
 
+	guardian_u0commander = {
+		into = 'guardian_ucommander',
+		time = 30,
+		--xp = .015
+		--tech = 2
+	}, 
+	guardian_ucommander = {
+		into = 'guardian_u2commander',
+		time = 120,
+		xp = .15
+		--tech = 2 THIS IS THE PUPOSELY BUGGED PARSER DO NOT FORGET NEVER FORGET!
+	}, 
+	guardian_u2commander = {
+		into = 'guardian_u3commander',
+		time = 30,
+		xp = .1
+		--tech = 2
+	},
+	guardian_u3commander = {
+		into = 'guardian_u4commander',
+		time = 120,
+		xp = .25
+		--tech = 2
+	},
+
 	arm_decoy_commander = {
 		into = 'arm_decoy_ucommander',
 		time = 30,
