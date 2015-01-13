a74d3c804e1abd626c95f9d9c6ac1365b5ca84dd
springjools <springjools@gmail.com>
2015-01-13 22:26:28 +0000

    Fix TLL metal makers

--- luarules/configs/maker_defs.lua
+++ luarules/configs/maker_defs.lua
@@ -8,6 +8,12 @@ local convertCapacities = {
 		[UnitDefNames.core_metal_maker.id]  = { c = 60, e = 1/60 }, -- Normal
 		[UnitDefNames.core_floating_metal_maker.id]  = { c = 180, e = 1/60 }, -- Floating
 		[UnitDefNames.core_moho_metal_maker.id]  = { c = 720, e = 1/60 }, -- Moho
+		
+		--TLL
+		[UnitDefNames.lost_metal_maker.id]  = { c = 60, e = 1/60 }, -- Normal
+		[UnitDefNames.lost_floating_metal_maker.id]  = { c = 180, e = 1/60 }, -- Floating
+		[UnitDefNames.lost_moho_metal_maker.id]  = { c = 960, e = 1/60 }, -- Moho
+		
     }
 
 return convertCapacities
