07d87aa472dfc3389c4b21a46ad2639a3d14317d
johnbaraniecki@hotmail.com <johnbaraniecki@hotmail.com@bcb9ecf5-47d0-65c3-ce09-e8bdce616090>
2015-01-20 21:37:13 +0000

    Buffed Superior Battleships by 50 percent in terms of hit points. Changed description of Arms original Spider to match role. Disabled Commando for xtaids. Replaced with new tech 1 kbot called Gizmo, which builds economy and defense units, can repair others, and defend itself. Also can build a unique unit called Pestilence, another xtaid only unit.

--- trunk/gamedata/unitdefs_post.lua
+++ trunk/gamedata/unitdefs_post.lua
@@ -361,7 +361,7 @@ if (modOptions and not tobool(modOptions.xtaidunits)) then
 	"corbyp", "justice", "corpur", "astank", "corjar", "core_egg_shell",
 	"corscrew", "corcrw", "corfff", "screamer", "corebfortns", "corhback",
 	"commando", "corevashp", "core_emperor", "cor_jumbo", "scorp3g",
-	"nsaclash", "tadg06"
+	"nsaclash", "tadg06", "core_gizmo"
   })
 end
 --------------------------------------------------------------------------------
