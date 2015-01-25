917f849204def513bf448430f1ccc19bd62639db
springjools <springjools@gmail.com>
2015-01-25 02:21:36 +0000

    Fix yardmaps and some gadgets that require lost units to be listed.

--- luarules/gadgets/unit_no_land_damage.lua
+++ luarules/gadgets/unit_no_land_damage.lua
@@ -23,29 +23,29 @@ end
 local GetUnitBasePosition = Spring.GetUnitBasePosition
 
 local NO_LAND_DAMAGE = {
+  --[WeaponDefNames['coramph_weapon1'].id] = true
+  --[WeaponDefNames['corseap_weapon1'].id] = true,
+  --[WeaponDefNames['corseap_weapon2'].id] = true,
+  --[WeaponDefNames['tawf009_weapon'].id] = true,
+  [WeaponDefNames['arm_torpedo'].id] = true,
   [WeaponDefNames['armair_torpedo'].id] = true,
   [WeaponDefNames['armatl_torpedo'].id] = true,
   [WeaponDefNames['armdepthcharge'].id] = true,
   [WeaponDefNames['armseap_weapon1'].id] = true,
   [WeaponDefNames['armseap_weapon2'].id] = true,
   [WeaponDefNames['armsmart_torpedo'].id] = true,
-  [WeaponDefNames['arm_torpedo'].id] = true,
+  [WeaponDefNames['boat_torpedo'].id] = true,
   [WeaponDefNames['coax_torpedo'].id] = true,
   [WeaponDefNames['corair_torpedo'].id] = true,
   [WeaponDefNames['coratl_torpedo'].id] = true,
-  [WeaponDefNames['coredepthcharge'].id] = true,
   [WeaponDefNames['core_torpedo'].id] = true,
-  --[WeaponDefNames['corseap_weapon1'].id] = true,
-  --[WeaponDefNames['corseap_weapon2'].id] = true,
+  [WeaponDefNames['coredepthcharge'].id] = true,
   [WeaponDefNames['corsmart_torpedo'].id] = true,
   [WeaponDefNames['corssub_weapon'].id] = true,
-  [WeaponDefNames['levi_torpedo'].id] = true,
-  [WeaponDefNames['boat_torpedo'].id] = true,
-  [WeaponDefNames['tank_torpedo'].id] = true,
   [WeaponDefNames['fibby_torpedo'].id] = true,
+  [WeaponDefNames['levi_torpedo'].id] = true,
   [WeaponDefNames['stupidsubs'].id] = true,
-  --[WeaponDefNames['tawf009_weapon'].id] = true,
-  --[WeaponDefNames['coramph_weapon1'].id] = true
+  [WeaponDefNames['tank_torpedo'].id] = true,
 }
 
 function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, 
