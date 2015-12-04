af17b1146b456c0fb50b0f2c58367596333ca7b2
springjools <springjools@gmail.com>
2015-12-04 02:16:00 +0200

    Add red console and update red framework and drawing
    Signed-off-by: springjools <springjools@gmail.com>

--- luaui/widgets/red_ui_framework.lua
+++ luaui/widgets/red_ui_framework.lua
@@ -1,20 +1,24 @@
 function widget:GetInfo()
 	return {
-	version   = "8",
+	version   = "9",
 	name      = "Red_UI_Framework",
 	desc      = "Red UI Framework",
-	author    = "Regret",
-	date      = "July 30, 2009", --last change August 29,2009
+	author    = "Regret (enhanced by Floris)",
+	date      = "29 may 2015",
 	license   = "GNU GPL, v2 or later",
 	layer     = -99999, --lowest go first
-	enabled   = false, --loaded by default
+	enabled   = true, --loaded by default
 	handler   = true, --access to handler
 	}
 end
 
+local useRoundedRectangles = true
+local roundedSizeMultiplier = 1
+local usedRoundedSize = roundedSize
+
 local TN = "Red"
 local DrawingTN = "Red_Drawing" --WG name for drawing function list
-local version = 8
+local version = 9
 
 local clock = os.clock
 local glGetTextWidth = gl.GetTextWidth
@@ -28,6 +32,7 @@ local Main = {} --boss table with a manabar
 local WidgetList = {} --list of widgets using the framework
 
 local LastProcessedWidget = "" --for debugging
+local inTweak = false
 
 local vsx,vsy = widgetHandler:GetViewSizes()
 if (vsx == 1) then --hax for windowed mode
@@ -37,6 +42,7 @@ function widget:ViewResize(viewSizeX, viewSizeY)
 	vsx,vsy = widgetHandler:GetViewSizes()
 	Main.vsx,Main.vsy = vsx,vsy
 	Main.Screen.vsx,Main.Screen.vsy = vsx,vsy
+	usedRoundedSize = 4 + math.floor((((vsx*vsy) / 900000))) * roundedSizeMultiplier
 end
 
 
@@ -81,105 +87,188 @@ end
 
 --Objects
 local F = {
-[1] = function(o) --rectangle
-	if (o.draw == false) then
-		return
-	end
-	
-	local color = o.color
-	local border = o.border	--color
-	local texturecolor = o.texturecolor
-	local captioncolor = o.captioncolor
-	
-	local texture = o.texture
-	local px,py,sx,sy = o.px,o.py,o.sx,o.sy	
-	
-	local alphamult = o.alphamult
-	if (alphamult~=nil) then
+	[1] = function(o) --rectangle
+		if (o.draw == false) then
+			return
+		end
+		
+		local color = o.color
+		local border = o.border	--color
+		local texturecolor = o.texturecolor
+		local captioncolor = o.captioncolor
+		local iconscale = 1
+		if o.iconscale ~= nil then
+			iconscale = o.iconscale
+		end
+		
+		local texture = o.texture
+		local px,py,sx,sy = o.px,o.py,o.sx,o.sy	
+		
+		local alphamult = o.alphamult
+		if (alphamult~=nil) then
+			if (color) then
+				color = copytable(color)
+				color[4] = o.color[4]*alphamult
+			end
+			if (border) then
+				border = copytable(border)
+				border[4] = o.border[4]*alphamult
+			end
+			if (captioncolor) then
+				captioncolor = copytable(captioncolor)
+				captioncolor[4] = o.captioncolor[4]*alphamult
+			end
+			if (texturecolor) then
+				texturecolor = copytable(texturecolor)
+				texturecolor[4] = o.texturecolor[4]*alphamult
+			else
+				texturecolor = {1,1,1,alphamult}
+			end
+		end
+		
 		if (color) then
-			color = copytable(color)
-			color[4] = o.color[4]*alphamult
+			Rect(px,py,sx,sy,color,iconscale)
 		end
-		if (border) then
-			border = copytable(border)
-			border[4] = o.border[4]*alphamult
+		
+		if (texture) then
+			TexRect(px,py,sx,sy,texture,texturecolor,iconscale)
 		end
-		if (captioncolor) then
-			captioncolor = copytable(captioncolor)
-			captioncolor[4] = o.captioncolor[4]*alphamult
+		
+		if (o.caption) then
+			local px2,py2 = px,py
+			local text = o.caption
+			local width = glGetTextWidth(text)
+			local linecount = getLineCount(text)
+			local fontsize = sx/width
+			local height = linecount*fontsize
+			if (height > sy) then
+				fontsize = sy/linecount
+				px2 = px2 + (sx - width*fontsize) /2 --center
+			else
+				py2 = py2 + (sy - height) /2 --center
+			end
+			Text(px2+((sx*(1-iconscale))/2),py2+((sy*(1-iconscale))/2),fontsize*iconscale,text,o.options,captioncolor)
+			o.autofontsize = fontsize
 		end
-		if (texturecolor) then
-			texturecolor = copytable(texturecolor)
-			texturecolor[4] = o.texturecolor[4]*alphamult
-		else
-			texturecolor = {1,1,1,alphamult}
+		
+		if (border) then --todo: border styles
+			Border(px,py,sx,sy,o.borderwidth,border)
 		end
-	end
-	
-	if (color) then
-		Rect(px,py,sx,sy,color)
-	end
-	
-	if (texture) then
-		TexRect(px,py,sx,sy,texture,texturecolor)
-	end
-	
-	if (o.caption) then
-		local px2,py2 = px,py
-		local text = o.caption
-		local width = glGetTextWidth(text)
-		local linecount = getLineCount(text)
-		local fontsize = sx/width
-		local height = linecount*fontsize
-		if (height > sy) then
-			fontsize = sy/linecount
-			px2 = px2 + (sx - width*fontsize) /2 --center
-		else
-			py2 = py2 + (sy - height) /2 --center
-		end
-		Text(px2,py2,fontsize,text,o.options,captioncolor)
-		o.autofontsize = fontsize
-	end
-	
-	if (border) then --todo: border styles
-		Border(px,py,sx,sy,o.borderwidth,border)
-	end
-end,
+	end,
 
-[2] = function(o) --text
-	if (o.draw == false) then
-		return
-	end
-	
-	local color = o.color
-	local captioncolor = o.captioncolor
-	
-	local alphamult = o.alphamult
-	if (alphamult~=nil) then
+	[2] = function(o) --text
+		if (o.draw == false) then
+			return
+		end
+		
+		local color = o.color
+		local captioncolor = o.captioncolor
+		
+		local alphamult = o.alphamult
+		if (alphamult~=nil) then
+			if (color) then
+				color = copytable(color)
+				color[4] = o.color[4]*alphamult
+			elseif (captioncolor) then
+				captioncolor = copytable(captioncolor)
+				captioncolor[4] = o.captioncolor[4]*alphamult
+			end
+		end
+		
+		local px,py = o.px,o.py	
+		local fontsize = o.fontsize
+		
+		Text(px,py,fontsize,o.caption,o.options,color or captioncolor)
+	end,
+
+	[3] = function(o) --area
+		--plain dummy
+	end,
+
+	[4] = function(o) --rectangle rounded
+		if (o.draw == false) then
+			return
+		end
+		
+		local color = o.color
+		local border = o.border	--color
+		local texturecolor = o.texturecolor
+		local captioncolor = o.captioncolor
+		local iconscale = 1
+		if o.iconscale ~= nil then
+			iconscale = o.iconscale
+		end
+		
+		local texture = o.texture
+		local px,py,sx,sy = o.px,o.py,o.sx,o.sy	
+		
+		local alphamult = o.alphamult
+		if (alphamult~=nil) then
+			if (color) then
+				color = copytable(color)
+				color[4] = o.color[4]*alphamult
+			end
+			if (border) then
+				border = copytable(border)
+				border[4] = o.border[4]*alphamult
+			end
+			if (captioncolor) then
+				captioncolor = copytable(captioncolor)
+				captioncolor[4] = o.captioncolor[4]*alphamult
+			end
+			if (texturecolor) then
+				texturecolor = copytable(texturecolor)
+				texturecolor[4] = o.texturecolor[4]*alphamult
+			else
+				texturecolor = {1,1,1,alphamult}
+			end
+		end
 		if (color) then
-			color = copytable(color)
-			color[4] = o.color[4]*alphamult
-		elseif (captioncolor) then
-			captioncolor = copytable(captioncolor)
-			captioncolor[4] = o.captioncolor[4]*alphamult
+			local roundedSize = usedRoundedSize
+			if o.roundedsize ~= nil then
+				roundedSize = o.roundedsize
+			end
+			RectRound(px,py,sx,sy,color,roundedSize,iconscale)
+			if o.glone ~= nil and o.glone > 0 then
+				RectRound(px,py,sx,sy,{color[1],color[2],color[3],o.glone},roundedSize,iconscale,true)
+			end
 		end
-	end
-	
-	local px,py = o.px,o.py	
-	local fontsize = o.fontsize
-	
-	Text(px,py,fontsize,o.caption,o.options,color or captioncolor)
-end,
-
-[3] = function(o) --area
-	--plain dummy
-end,
+		
+		if (texture) then
+			TexRect(px,py,sx,sy,texture,texturecolor,iconscale)
+		end
+		
+		if (o.caption) then
+			local px2,py2 = px,py
+			local text = o.caption
+			local width = glGetTextWidth(text)
+			local linecount = getLineCount(text)
+			local fontsize = sx/width
+			local height = linecount*fontsize
+			if (height > sy) then
+				fontsize = sy/linecount
+				px2 = px2 + (sx - width*fontsize) /2 --center
+			else
+				py2 = py2 + (sy - height) /2 --center
+			end
+			Text(px2+((sx*(1-iconscale))/2),py2+((sy*(1-iconscale))/2),fontsize*iconscale,text,o.options,captioncolor)
+			o.autofontsize = fontsize
+		end
+		
+		if (border) then --todo: border styles
+			--Border(px,py,sx,sy,o.borderwidth,border)
+		end
+	end,
 }
+if useRoundedRectangles == false then
+	F[4] = F[1]
+end
 
 local otypes = {
 	["rectangle"] = 1,
 	["text"] = 2,
 	["area"] = 3,
+	["rectanglerounded"] = 4,
 }
 -------------------------
 
@@ -253,11 +342,13 @@ end
 
 local WheelState = nil
 function widget:MouseWheel(up,value) --up = true/false , value = -1/1
+	if Spring.IsGUIHidden() then return end
 	WheelState = up
 	return dropWheel
 end
 
 function widget:MousePress(mx,my,mb)
+	if Spring.IsGUIHidden() then return end
 	if (type(dropClick)=="table") then
 		for i=1,#dropClick do
 			if (dropClick[i] == mb) then
@@ -275,6 +366,7 @@ end
 
 local LastMouseState = {sGetMouseState()}
 local function handleMouse()
+	if Spring.IsGUIHidden() then return end
 	--reset status
 	dropClick = false
 	dropWheel = false
@@ -348,78 +440,80 @@ local function processMouseEvents(o)
 	
 	if (o.movable) then
 		for i=1,#o.movable do
-			if (not o.wasclicked) then
-				if (Mouse[o.movable[i]][2]) then
-					if (isInRect(Mouse.x,Mouse.y,o)) then
-						o.checkedformouse = true
-						
-						o.wasclicked = {o.px - Mouse.x,o.py - Mouse.y}
-						Mouse[o.movable[i]][2] = nil --so only topmost area will get the event
-					end
-				end
-			else
-				local newpx = Mouse.x + o.wasclicked[1]
-				local newpy = Mouse.y + o.wasclicked[2]
-				
-				if (o.obeyscreenedge) then
-					if (newpx<0) then newpx = 0 end
-					if (newpy<0) then newpy = 0 end
-					if (newpx>(vsx-o.sx)) then newpx = vsx-o.sx end
-					if (newpy>(vsy-o.sy)) then newpy = vsy-o.sy end
-				elseif (o.movablearea) then
-					if (newpx<o.movablearea.px) then newpx = o.movablearea.px end
-					if (newpy<o.movablearea.py) then newpy = o.movablearea.py end
-					if (newpx>((o.movablearea.px+o.movablearea.sx)-o.sx)) then newpx = (o.movablearea.px+o.movablearea.sx)-o.sx end
-					if (newpy>((o.movablearea.py+o.movablearea.sy)-o.sy)) then newpy = (o.movablearea.py+o.movablearea.sy)-o.sy end
-				elseif (o.minpx) then
-					if (newpx<o.minpx) then newpx = o.minpx end
-					if (newpy<o.minpy) then newpy = o.minpy end
-					if (newpx>(o.maxpx-o.sx)) then newpx = o.maxpx-o.sx end
-					if (newpy>(o.maxpy-o.sy)) then newpy = o.maxpy-o.sy end
-				end
-				
-				local changex = newpx-o.px
-				local changey = newpy-o.py
-				if (o.movableslaves) then
-					for j=1,#o.movableslaves do
-						local s = o.movableslaves[j]
-						local snewpx = s.px - (o.px - newpx)
-						local snewpy = s.py - (o.py - newpy)
-						
-						if (s.obeyscreenedge) then
-							if (snewpx<0) then snewpx = 0 end
-							if (snewpy<0) then snewpy = 0 end
-							if (snewpx>(vsx-s.sx)) then snewpx = vsx-s.sx end
-							if (snewpy>(vsy-s.sy)) then snewpy = vsy-s.sy end
-						elseif (s.movablearea and (s.movablearea~=o)) then --disregard self to prevent a bug
-							if (snewpx<s.movablearea.px) then snewpx = s.movablearea.px end
-							if (snewpy<s.movablearea.py) then snewpy = s.movablearea.py end
-							if (snewpx>((s.movablearea.px+s.movablearea.sx)-s.sx)) then snewpx = (s.movablearea.px+s.movablearea.sx)-s.sx end
-							if (snewpy>((s.movablearea.py+s.movablearea.sy)-s.sy)) then snewpy = (s.movablearea.py+s.movablearea.sy)-s.sy end
-						elseif (s.minpx) then
-							if (snewpx<s.minpx) then snewpx = s.minpx end
-							if (snewpy<s.minpy) then snewpy = s.minpy end
-							if (snewpx>(s.maxpx-s.sx)) then snewpx = s.maxpx-s.sx end
-							if (snewpy>(s.maxpy-s.sy)) then snewpy = s.maxpy-s.sy end
+			if inTweak or (o.onlyTweakUi ~= nil and o.onlyTweakUi == false) then
+				if (not o.wasclicked) then
+					if (Mouse[o.movable[i]][2]) then
+						if (isInRect(Mouse.x,Mouse.y,o)) then
+							o.checkedformouse = true
+							
+							o.wasclicked = {o.px - Mouse.x,o.py - Mouse.y}
+							Mouse[o.movable[i]][2] = nil --so only topmost area will get the event
 						end
-						
-						local schangex = snewpx-s.px
-						local schangey = snewpy-s.py
-						
-						if (math.abs(changex)>math.abs(schangex)) then changex = schangex end
-						if (math.abs(changey)>math.abs(schangey)) then changey = schangey end
 					end
-					for j=1,#o.movableslaves do --move slaves
-						local s = o.movableslaves[j]
-						s.px = s.px+changex
-						s.py = s.py+changey
+				else
+					local newpx = Mouse.x + o.wasclicked[1]
+					local newpy = Mouse.y + o.wasclicked[2]
+					
+					if (o.obeyscreenedge) then
+						if (newpx<0) then newpx = 0 end
+						if (newpy<0) then newpy = 0 end
+						if (newpx>(vsx-o.sx)) then newpx = vsx-o.sx end
+						if (newpy>(vsy-o.sy)) then newpy = vsy-o.sy end
+					elseif (o.movablearea) then
+						if (newpx<o.movablearea.px) then newpx = o.movablearea.px end
+						if (newpy<o.movablearea.py) then newpy = o.movablearea.py end
+						if (newpx>((o.movablearea.px+o.movablearea.sx)-o.sx)) then newpx = (o.movablearea.px+o.movablearea.sx)-o.sx end
+						if (newpy>((o.movablearea.py+o.movablearea.sy)-o.sy)) then newpy = (o.movablearea.py+o.movablearea.sy)-o.sy end
+					elseif (o.minpx) then
+						if (newpx<o.minpx) then newpx = o.minpx end
+						if (newpy<o.minpy) then newpy = o.minpy end
+						if (newpx>(o.maxpx-o.sx)) then newpx = o.maxpx-o.sx end
+						if (newpy>(o.maxpy-o.sy)) then newpy = o.maxpy-o.sy end
 					end
-				end
-				o.px = o.px+changex --move self
-				o.py = o.py+changey
-				if (Mouse[o.movable[i]][3]) then
-					o.wasclicked = nil
-					Mouse[o.movable[i]][3] = nil --so only topmost area will get the event
+					
+					local changex = newpx-o.px
+					local changey = newpy-o.py
+					if (o.movableslaves) then
+						for j=1,#o.movableslaves do
+							local s = o.movableslaves[j]
+							local snewpx = s.px - (o.px - newpx)
+							local snewpy = s.py - (o.py - newpy)
+							
+							if (s.obeyscreenedge) then
+								if (snewpx<0) then snewpx = 0 end
+								if (snewpy<0) then snewpy = 0 end
+								if (snewpx>(vsx-s.sx)) then snewpx = vsx-s.sx end
+								if (snewpy>(vsy-s.sy)) then snewpy = vsy-s.sy end
+							elseif (s.movablearea and (s.movablearea~=o)) then --disregard self to prevent a bug
+								if (snewpx<s.movablearea.px) then snewpx = s.movablearea.px end
+								if (snewpy<s.movablearea.py) then snewpy = s.movablearea.py end
+								if (snewpx>((s.movablearea.px+s.movablearea.sx)-s.sx)) then snewpx = (s.movablearea.px+s.movablearea.sx)-s.sx end
+								if (snewpy>((s.movablearea.py+s.movablearea.sy)-s.sy)) then snewpy = (s.movablearea.py+s.movablearea.sy)-s.sy end
+							elseif (s.minpx) then
+								if (snewpx<s.minpx) then snewpx = s.minpx end
+								if (snewpy<s.minpy) then snewpy = s.minpy end
+								if (snewpx>(s.maxpx-s.sx)) then snewpx = s.maxpx-s.sx end
+								if (snewpy>(s.maxpy-s.sy)) then snewpy = s.maxpy-s.sy end
+							end
+							
+							local schangex = snewpx-s.px
+							local schangey = snewpy-s.py
+							
+							if (math.abs(changex)>math.abs(schangex)) then changex = schangex end
+							if (math.abs(changey)>math.abs(schangey)) then changey = schangey end
+						end
+						for j=1,#o.movableslaves do --move slaves
+							local s = o.movableslaves[j]
+							s.px = s.px+changex
+							s.py = s.py+changey
+						end
+					end
+					o.px = o.px+changex --move self
+					o.py = o.py+changey
+					if (Mouse[o.movable[i]][3]) then
+						o.wasclicked = nil
+						Mouse[o.movable[i]][3] = nil --so only topmost area will get the event
+					end
 				end
 			end
 		end
@@ -572,12 +666,19 @@ function widget:Shutdown()
 	end
 end
 
+function widget:TweakDrawScreen()
+	inTweak = true
+end
+function widget:DrawScreen()
+	inTweak = false
+end
+
 local hookedtodrawing = false
 local fc = 0 --framecount
 function widget:Update()
+	
 	Main.tooltip = nil
 	handleMouse()
-	
 	--flush deactivated widgets
 	fc=fc+1
 	if (fc > 200) then
@@ -607,6 +708,7 @@ function widget:Update()
 			end
 			Color = X.Color
 			Rect = X.Rect
+			RectRound = X.RectRound
 			TexRect = X.TexRect
 			Border = X.Border
 			Text = X.Text
