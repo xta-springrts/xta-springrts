9c744fd6f26bd87a3f7a3af2120b9c8451e6707f
jools <springjools@gmail.com>
2017-07-22 23:19:15 +0300

    Some new protips (poems)

--- luaintro/Addons/configs/protips_defs.lua
+++ luaintro/Addons/configs/protips_defs.lua
@@ -30,6 +30,10 @@ local tips = {
 	'The Commander is visible under water if the enemy has sonar, or if even the smallest piece of his antennae appear above the surface',
 	'When units gain experience, they become stronger, faster and more accurate',
 	'Use crawling bombs to strike the enemy where he doesn\'t expect it',
+	'Do not go gentle into that good night, Old age should burn and rave at close of day; Rage, rage against the dying of the light',
+	'Though wise men at their end know dark is right, Because their words had forked no lightning they Do not go gentle into that good night.',
+	'Good men, the last wave by, crying how bright Their frail deeds might have danced in a green bay, Rage, rage against the dying of the light.',
+	'Wild men who caught and sang the sun in flight, And learn, too late, they grieved it on its way, Do not go gentle into that good night.',
 }
 
 return tips
