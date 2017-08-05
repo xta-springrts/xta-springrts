ee925cab6f3a722e2ad9b3e203266c01ba92d117
restrictor <d2.bijkerk@student.vu.nl>
2017-08-05 18:51:50 +0200

    little fish big fish swimming in the water

--- luarules/configs/gaia_dynamic_critters_config.lua
+++ luarules/configs/gaia_dynamic_critters_config.lua
@@ -14,6 +14,17 @@ local critterConfig = {
 	},
 },
 
+["trefoil"] = {
+[1] = {
+		["prey"] = { spawnBox = {x1=5666, z1=4888, x2=8000, z2=6400}, unitNames = {["critter_goldfish"]=10} },
+		["pred"] = { spawnBox = {x1=5666, z1=4888, x2=8000, z2=6400}, unitNames = {["critter_goldfish_big"]=10} },
+	},
+[2] = {
+		["prey"] = { spawnBox = {x1=3400, z1=300, x2=5000, z2=1500}, unitNames = {["critter_goldfish"]=10} },
+		["pred"] = { spawnBox = {x1=3400, z1=300, x2=5000, z2=1500}, unitNames = {["critter_goldfish_big"]=10} },
+	},
+},
+
 }
 
 return critterConfig
