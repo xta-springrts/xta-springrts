3dbc5aed815f9d716001493797b6aabca45f8978
restrictor <d2.bijkerk@student.vu.nl>
2017-09-03 15:53:49 +0200

    Fixed gadget water sounds, but used widget callins!

--- luarules/gadgets/unit_water_sounds_xta.lua
+++ luarules/gadgets/unit_water_sounds_xta.lua
@@ -1,3 +1,4 @@
+
 function gadget:GetInfo()
   return {
     name = "soundy units in water",
@@ -11,12 +12,8 @@ function gadget:GetInfo()
 end
 
 -- TODO
--- Unit should not make noice when they are building, gaurding, ... etc (fix: commander)
 -- Add a bubbles animation when going submerge (with sound)
--- Building hover platforms, tidels, metal makers, radar and ... splashes (these should be removed from soundfx)
--- Add the option to make crittes in water LOS only visible
-
-
+-- Add the option to have only in LOS sounds (maybe)?
 
 local IsWaterDragUnit = {
 	["kbotsf2"] 	= true, 
@@ -48,7 +45,6 @@ local IsHover = {
 --Speed-ups
 local abs						= math.abs
 local random 					= math.random           
---local GetLocalTeamID			= Spring.MY_UNITS
 local PlaySoundFile				= Spring.PlaySoundFile
 local GetUnitPosition			= Spring.GetUnitPosition
 local GetGameFrame				= Spring.GetGameFrame
@@ -77,26 +73,16 @@ local pairs						= pairs
 
 
 function gadget:Initialize()
-	
-	-- after reloading and startup add units in water back to the table
 	for _, unitID in ipairs(GetAllUnits()) do
 		local unitDefID = GetUnitDefID(unitID)
-		local unitTeam = GetUnitTeam(unitID)
 		local x,y,z = GetUnitPosition(unitID)
-		
-		if y < 0 then --and (localTeamID == unitTeam) then
+		if y < 0 then 
 			if IsWaterDragUnit[tostring(UnitDefs[unitDefID].moveDef.name)] ~= nil then
 				waterUnits[unitID] = true
 			end
 		end
 	end
 	Spring.Log("gadget",LOG.INFO,"Unit water sounds (XTA) loaded.")
-	
-end
-
-
-function delay(unitID, submerge)
-	
 end
 
 
@@ -108,19 +94,20 @@ function dragSound(unitID)
 		local level = GetUnitDefDimensions(unitDefID)["height"] + y
 		local velocity = GetUnitVelocity(unitID)
 		
-		if velocity > 0.0001 then 
-			--not submersive
-			if  (level > 0) then
-				
-				PlaySoundFile(WATER_MOVEMENT,volume * 4 * (level/height),x,y,x,0,0,0,'battle')
-				
-			--submersive
+		if y < 0 then
+			if abs(velocity) > 0.0001 then 
+				if  (level > 0) then				
+					PlaySoundFile(WATER_MOVEMENT,volume * 4 * (level/height),x,y,x,0,0,0,'battle')
+				else
+					PlaySoundFile(UNDERWATER_MOVING, volume + 4 / (1-level),x,y,x,0,0,0,'battle')
+				end
 			else
-				PlaySoundFile(UNDERWATER_MOVING, volume + 4 / (1-level),x,y,x,0,0,0,'battle')
+				notMovingWaterUnits[unitID] = nil
+				waterUnits[unitID] = true
 			end
 		else
 			notMovingWaterUnits[unitID] = nil
-			waterUnits[unitID] = true
+			waterUnits[unitID] = nil
 		end
 	end
 end
@@ -140,19 +127,14 @@ end
 
 
 function gadget:GameFrame(f)
-	
 	updateDelaySounds(delaySounds) 
-	
 	if f%SECOND == 0 then
-	
 		if f%2 == 0 then
-			
-			-- updating 
 			
 			-- checking if moving units are still moving
 			for unitID,v in pairs(notMovingWaterUnits) do
 				local velocity = GetUnitVelocity(unitID)
-				if velocity > 0.0001 then
+				if abs(velocity) > 0.0001 then
 					notMovingWaterUnits[unitID] = nil
 					waterUnits[unitID] = true
 				end
@@ -168,22 +150,13 @@ function gadget:GameFrame(f)
 end
 
 
-function gadget:UnitCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOpts, cmdTag)  
-	if notMovingWaterUnits[unitID] then
-		if cmdID == CMD_MOVE then
-			local x,y,z = GetUnitPosition(unitID)			
-			waterUnits[unitID] = true
-			notMovingWaterUnits[unitID] = nil
-		end
-	end
-end
-
 function gadget:UnitIdle(unitID, unitDefID, unitTeam)
 	if waterUnits[unitID] then
 		notMovingWaterUnits[unitID] = true
 		waterUnits[unitID] = nil
 	end
 end
+
 
 function gadget:UnitDestroyed(unitID, unitDefID, teamID)	
 	if waterUnits[unitID] or notMovingWaterUnits[unitID] then
@@ -213,14 +186,11 @@ end
 
 
 function gadget:UnitEnteredWater(unitID, unitDefID, unitTeam)
-
-	--all units splash comming in to the water
 	local x,y,z = GetUnitPosition(unitID)
 	local unitTeam = GetUnitTeam(unitID)
 	local velocity = GetUnitVelocity(unitID)
 	if unitTeam ~= GaiaTeamID then
-	--make sure units created in water dont splash (Still they do)
-		if y > -10 and velocity > 0.001 then
+		if abs(velocity) > 0.001 then
 			local radius = GetUnitDefDimensions(unitDefID)["radius"]
 			if IsHover[tostring(UnitDefs[unitDefID].moveDef.name)] then
 				PlaySoundFile(HOVER_PLASH, volume * math.abs((radius-10))/60 ,x,y,z,0,0,0,'battle')
@@ -228,10 +198,7 @@ function gadget:UnitEnteredWater(unitID, unitDefID, unitTeam)
 				PlaySoundFile(NONHOVER_SPLASH, volume * math.abs((radius-10))/60,x,y,z,0,0,0,'battle')
 			end
 		end
-		
-		--store soundy units in water
 		if IsWaterDragUnit[tostring(UnitDefs[unitDefID].moveDef.name)] ~= nil then
-			--Echo(UnitDefs[unitDefID].name)
 			waterUnits[unitID] = true
 		end
 	end
@@ -239,8 +206,6 @@ end
 
 
 function gadget:UnitLeftWater(unitID, unitDefID, unitTeam)
-	
 	notMovingWaterUnits[unitID] = nil
 	waterUnits[unitID] = nil
-	
 end
